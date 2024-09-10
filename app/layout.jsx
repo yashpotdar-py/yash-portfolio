6bfd3ebb1a7951d917cca69ebaab6e182ee35110
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-09-10 22:02:34 +0530

    Creating a stairs animation

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import "./globals.css";
 // components
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         className={`${jetBrainsMono.variable} ${jetBrainsMono.variable} antialiased`}
       >
         <Header />
+        <StairTransition />
         <PageTransition>{children}</PageTransition>
       </body>
     </html>
---
+++ components/Stairs.jsx
@@ -0,0 +1,45 @@
+import { motion } from "framer-motion";
+
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+const reverseIndex = (index) => {
+  const totalSteps = 8;
+  return totalSteps - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+      {/* Rendering Stairs using 6 divs, each representing a single step */}
+      {[...Array(8)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative pointer-events-none"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Stairs;
