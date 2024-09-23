8cfd75d3c92effa600cca906e33eae5c6f99b437
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-09-23 22:27:34 +0530

    Adding data to Resume page

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -2,16 +2,12 @@
 
 import {
   SiPython,
-  SiDatabricks,
   SiJupyter,
   SiTensorflow,
   SiScikitlearn,
   SiPytorch,
   SiKeras,
-  SiOpenai,
-  SiRaspberrypi,
   SiArduino,
-  SiBeautifulsoup,
   SiReact,
   SiNextdotjs,
   SiTailwindcss,
@@ -20,6 +16,7 @@ import {
   SiGithub,
   SiLinux,
 } from "react-icons/si";
+import { RiRobot3Line } from "react-icons/ri";
 import {
   Tooltip,
   TooltipContent,
@@ -34,9 +31,9 @@ import { motion } from "framer-motion";
 const aboutData = {
   title: "About Me",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam suscipit beatae, \
-     obcaecati, ullam laborum tenetur provident mollitia consectetur ipsam nam eligendi porro \
-     nemo illum cupiditate omnis id quas assumenda!",
+    "I am Yash Potdar, a passionate learner with a diverse set of interests and a strong foundation in AI/ML and a strong interest in Cybersecurity. \
+    Currently a student, I am committed to gaining hands-on experience through various projects, internships and freelance opportunities. \
+    My goal is to continually grow and apply my skills in meaningful ways, while embracing challenges that push me to innovate and excel.",
   info: [
     {
       fieldName: "Name",
@@ -62,12 +59,6 @@ const aboutData = {
       fieldName: "Email",
       fieldValue: "yashyogeshpotdar7@outlook.com",
     },
-    // {
-    //   fieldName: "Languages",
-    //   fieldValue:
-    //     "Marathi (Native), Hindi (Proficient), English (Proficient), Japanese (Beginner)",
-    // },
-    ,
   ],
 };
 
@@ -76,9 +67,11 @@ const exprienceData = {
   icon: "/assets/resume/badge.svg",
   title: "Experience",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam suscipit beatae, \
-     obcaecati, ullam laborum tenetur provident mollitia consectetur ipsam nam eligendi porro \
-     nemo illum cupiditate omnis id quas assumenda!",
+    "My experience encompasses a diverse range of roles that have shaped my skills in real-world applications. \
+    From internships to part for student clubs like {<span classname='text-accent'>Google Developer's Group</span>}, \
+    I have worked in dynamic environments, applying my technical expertise in machine learning and beyond. These \
+    experiences have refined my problem-solving capabilities, strengthened my teamwork, and given me a deep \
+    understanding of professional responsibilities and expectations.",
   items: [
     {
       company: "Google Developers Group",
@@ -108,9 +101,10 @@ const educationCertData = {
   icon: "/assets/resume/cap.svg",
   title: "Eduation & Certifications",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam suscipit beatae, \
-     obcaecati, ullam laborum tenetur provident mollitia consectetur ipsam nam eligendi porro \
-     nemo illum cupiditate omnis id quas assumenda!",
+    "I hold a solid educational foundation, complemented by industry-recognized certifications that underscore my \
+    commitment to professional development. From AI&DS to specialized courses in cybersecurity and web technology, \
+    my academic journey has equipped me with a robust skill set. These qualifications not only validate my knowledge but \
+    also reflect my drive to stay updated with the latest advancements in my field.",
   items: [
     {
       institution: "AISSMS Institute of Information Technology, Pune",
@@ -161,9 +155,10 @@ const educationCertData = {
 const skillsData = {
   title: "Skills",
   description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aliquam suscipit beatae, \
-     obcaecati, ullam laborum tenetur provident mollitia consectetur ipsam nam eligendi porro \
-     nemo illum cupiditate omnis id quas assumenda!",
+    "I bring a well-rounded skill set, combining technical expertise with strong analytical thinking. \
+    My key skills include AI/ML techniques, problem-solving, and proficiency in various programming languages. \
+    Additionally, I excel in communication, teamwork, and adaptability—traits that have helped me thrive in diverse professional \
+    environments. Together, these skills form a strong foundation for tackling complex challenges and delivering innovative solutions.",
   skillList: [
     {
       icon: <SiPython />,
@@ -171,18 +166,28 @@ const skillsData = {
       level: "Advanced",
     },
     {
-      icon: <SiDatabricks />,
-      name: "Data Science",
+      icon: <RiRobot3Line />,
+      name: "AI/ML",
+      level: "Intermediate",
+    },
+    {
+      icon: <SiLinux />,
+      name: "Linux",
       level: "Intermediate",
     },
     {
       icon: <SiJupyter />,
-      name: "Jupyter",
+      name: "Data Science",
       level: "Intermediate",
     },
     {
       icon: <SiTensorflow />,
       name: "Tensorflow",
+      level: "Intermediate",
+    },
+    {
+      icon: <SiKeras />,
+      name: "Keras",
       level: "Intermediate",
     },
     {
@@ -201,14 +206,34 @@ const skillsData = {
       level: "Intermediate",
     },
     {
-      icon: <SiLinux />,
-      name: "Linux",
+      icon: <SiKalilinux />,
+      name: "Kali Linux",
       level: "Intermediate",
     },
     {
-      icon: <SiOpenai />,
-      name: "AI/ML",
+      icon: <SiHackaday />,
+      name: "Ethical Hacking",
       level: "Intermediate",
+    },
+    {
+      icon: <SiArduino />,
+      name: "Arduino",
+      level: "Beginner",
+    },
+    {
+      icon: <SiReact />,
+      name: "React",
+      level: "Intermidiate",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "Next.JS",
+      level: "Beginner",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwind CSS",
+      level: "Beginner",
     },
   ],
 };
@@ -225,7 +250,7 @@ const Resume = () => {
     >
       <div className="container mx-auto">
         <Tabs
-          defaultValue="about-me"
+          defaultValue="skills"
           className="flex flex-col xl:flex-row gap-[60px]"
         >
           <TabsList className="flex flex-col gap-6 w-full max-w-[380px] mx-auto xl:mx-0 rounded">
@@ -244,33 +269,41 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px]">
                 <div className="flex flex-col gap-[30px] text-center xl:text-left">
                   <h3 className="text-4xl font-bold ">{skillsData.title}</h3>
-                  <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  <p className="max-w-[920px] text-white/60 mx-auto xl:mx-0">
                     {skillsData.description}
                   </p>
+                  <span className="text-accent">
+                    Hover over the icons to see more details.
+                  </span>
                 </div>
-                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
-                  {skillsData.skillList.map((skill, index) => {
-                    return (
-                      <li key={index} className="">
-                        <TooltipProvider className="" delayDuration={100}>
-                          <Tooltip className="">
-                            <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
-                              <div className="text-6xl group-hover:text-accent transition-all duration-300">
-                                {skill.icon}
-                              </div>
-                            </TooltipTrigger>
-                            <TooltipContent className="">
-                              <p className="capitalize">{skill.name}</p>
-                              <span className="text-accent-hover">
-                                {skill.level}
-                              </span>
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      </li>
-                    );
-                  })}
-                </ul>
+                <ScrollArea className="h-[400px]">
+                  <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:gap-[30px] gap-4">
+                    {skillsData.skillList.map((skill, index) => {
+                      return (
+                        <li key={index} className="">
+                          <TooltipProvider className="" delayDuration={100}>
+                            <Tooltip className="">
+                              <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                                <div className="text-6xl group-hover:text-accent transition-all duration-300 items-center flex flex-col justify-between">
+                                  <span>{skill.icon}</span>
+                                  <p className="text-sm mt-8 capitalize">
+                                    {skill.name}
+                                  </p>
+                                </div>
+                              </TooltipTrigger>
+                              <TooltipContent className="">
+                                <p className="capitalize">{skill.name}</p>
+                                <span className="text-accent-hover">
+                                  {skill.level}
+                                </span>
+                              </TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </ScrollArea>
               </div>
             </TabsContent>
 
@@ -280,7 +313,7 @@ const Resume = () => {
                 <h3 className="text-4xl font-bold">
                   {educationCertData.title}
                 </h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                <p className="max-w-[920px] text-white/60 mx-auto xl:mx-0">
                   {educationCertData.description}
                 </p>
                 <ScrollArea className="h-[400px]">
@@ -313,9 +346,20 @@ const Resume = () => {
             <TabsContent value="experience" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold ">{exprienceData.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
-                  {exprienceData.description}
+                <p className="max-w-[920px] text-white/60 mx-auto xl:mx-0">
+                  My experience encompasses a diverse range of roles that have
+                  shaped my skills in real-world applications. From internships
+                  to part for student clubs like{" "}
+                  <span className="text-accent">
+                    Google Developer&apos;s Group
+                  </span>
+                  , I have worked in dynamic environments, applying my technical
+                  expertise in machine learning and beyond. These experiences
+                  have refined my problem-solving capabilities, strengthened my
+                  teamwork, and given me a deep understanding of professional
+                  responsibilities and expectations.
                 </p>
+
                 <ScrollArea className="h-[400px]">
                   <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                     {exprienceData.items.map((item, index) => {
@@ -345,7 +389,7 @@ const Resume = () => {
             <TabsContent value="about-me" className="w-full">
               <div className="flex flex-col gap-[30px] text-center xl:text-left">
                 <h3 className="text-4xl font-bold">{aboutData.title}</h3>
-                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                <p className="max-w-[920px] text-white/60 mx-auto xl:mx-0">
                   {aboutData.description}
                 </p>
 
