cca520da526952c0d8c499f845382e1847b5dc2e
Yash Potdar <113132066+yashpotdar-py@users.noreply.github.com>
2024-10-30 23:17:25 +0530

    Minor Changes to Navigation Bar

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -20,12 +20,8 @@ const links = [
     path: "/resume",
   },
   {
-    name: "work",
-    path: "/work",
-  },
-  {
-    name: "contact",
-    path: "/contact",
+    name: "services",
+    path: "/services",
   },
 ];
 
