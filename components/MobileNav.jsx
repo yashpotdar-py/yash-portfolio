6e92d0e535140288f4a3cc8e107f306e3bed64b9
Yash Potdar <yashyogeshpotdar7@gmail.com>
2024-11-26 14:31:59 +0530

    Adding a resume

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -45,7 +45,7 @@ const MobileNav = () => {
           </Link>
         </div>
         {/* Navigation */}
-        <nav className="flex flex-col gap-8 p-4">
+        <Nav className="flex flex-col gap-8 p-4">
           {links.map((link, index) => {
             return (
               <Link
@@ -70,7 +70,7 @@ const MobileNav = () => {
           >
             <Button className="w-full">Hire Me</Button>
           </Link>
-        </nav>
+        </Nav>
       </SheetContent>
     </Sheet>
   );
